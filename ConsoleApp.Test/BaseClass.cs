9022fb7a9ec1af248c319446aad8a170119055e4
emanellari <132411421+emanellari@users.noreply.github.com>
2023-05-03 02:30:16 +0200

    Add files via upload

---
+++ ConsoleApp.Test/BaseClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Test
+{
+    public class BaseClass
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdate { get; set; }
+    }
+}
---
+++ ConsoleApp.Test/Doctors.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp.Test.Interfaces;
+
+namespace ConsoleApp.Test
+{
+    public class Doctors:IDataService
+    {
+        public string Name { get; set; }
+        public int ExperienceYears { get; set; }
+        public string LastName { get; set; }
+        public string SpecializationField { get; set; }
+        public int PhoneNumber { get; set; }
+
+        public string GetDataFromDatabase()
+        {
+            //throw new NotImplementedException();
+
+            //SQL
+
+            return "";
+        }
+    }
+}
